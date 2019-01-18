dbd6a386d709586ce774e67a091f0a252d018bca
Stephen Lake <stephen@closurecode.com>
2019-01-19 00:43:34 +0200

    Add alias for associating a property with a value

--- src/Traits/HasProperties.php
+++ src/Traits/HasProperties.php
@@ -18,4 +18,16 @@ trait HasProperties
         return $this->morphToMany(Property::class, 'propertyable')
                     ->withPivot('value');
     }
+
+    /**
+    * A simplified alias for attaching a property with a custom value.
+    *
+    * @return \Properties\Models\Property
+    */
+    public function assignProperty($propertyKey, $value)
+    {
+        $this->properties()->attach($propertyKey, ['value' => $value]);
+
+        return $this;
+    }
 }
