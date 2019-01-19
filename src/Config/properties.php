0362d192d02b7cfe6e6f364a4523264885999abe
Stephen Lake <stephen@closurecode.com>
2019-01-19 23:21:37 +0200

    Add config

---
+++ src/Config/properties.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Model
+    |--------------------------------------------------------------------------
+    |
+    | If you wish to extend the properties model with your own custom model,
+    | you'll need to specify your model here.
+    |
+    */
+    'model' => \Properties\Models\Property::class,
+];
