cd96147ea6c52fd4dcd53fb5d8530654876c1768
Stephen Lake <stephen@closurecode.com>
2019-04-09 16:11:16 +0200

    Add value attribute

--- src/Models/Propertyable.php
+++ src/Models/Propertyable.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Eloquent\Relations\MorphPivot;
 
 class Propertyable extends MorphPivot
 {
+
+  /**
+   * Mutate the value based on the property type.
+   *
+   * @param string $value
+   *
+   * @return mixed
+   */
+    public function getValueAttribute($value)
+    {
+        return $this->property->value;
+    }
+
     public function property()
     {
         return $this->belongsTo(config('properties.model', \Properties\Models\Property::class));
